d798893e6a821868768ea6ade5b332aaaae31d2f
pgs <2226984343@qq.com>
2022-10-21 00:54:45 +0800

    基本效果完成

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import '@/styles/index.scss'
 import './js/formValidation'
 import './js/eventEl'
+import QRCode from 'qrcodejs2'
+
+new QRCode('two-code', {
+    text: 'https://www.baidu.com',
+    width: 140,
+    height: 140,
+    correctLevel: QRCode.CorrectLevel.H
+})
--- src/js/eventEl.js
+++ src/js/eventEl.js
@@ -1,9 +1,63 @@
 import $ from 'jquery'
+import userIcon from '@/imgs/user-icon.png'
+import lockIcon from '@/imgs/lock-icon.png'
+import phoneIcon from '@/imgs/phone-icon.png'
+import verIcon from '@/imgs/ver-icon.png'
 
-$('.tab-item').on('click', function() {
-     
+changeTabHandler()
+changeCodeLogin()
+// 39秒后重发 --
+// $('.tab-item').on('click', function(e) {
+//     const idx = e.target.dataset.tab
+//     $('.tab-item').removeClass('mode-active')
+//     $(this).toggleClass('mode-active')
+// })
+
+
+
+$('.submit-btn').on('click', function(e) {
+    e.preventDefault()
 })
 
-$('.code-icon').on('click', function() {
-    
-})
+function changeTabHandler() {
+
+    $('.tab-item1').on('click', function() {
+        $('.sp1').show()
+        $('.get-code').hide()
+        $('.tab-item2').removeClass('mode-active')
+        $(this).addClass('mode-active')
+        $('.icon-cen1').attr({src: userIcon})
+        $('.icon-cen2').attr({src: lockIcon})
+        $('.pas-input1').attr({placeholder: '请输入账号'})
+        $('.pas-input2').attr({placeholder: '请输入密码'})
+    })
+
+    $('.tab-item2').on('click', function() {
+        $('.sp1').hide()
+        $('.get-code').show()
+        $('.tab-item1').removeClass('mode-active')
+        $(this).addClass('mode-active')
+        $('.icon-cen1').attr({src: phoneIcon})
+        $('.icon-cen2').attr({src: verIcon})
+        $('.pas-input1').attr({placeholder: '请输入手机号'})
+        $('.pas-input2').attr({placeholder: '请输入验证码'})
+    })
+} 
+
+function changeCodeLogin() {
+
+    $('.code-icon').on('click', function() {
+        $(this).hide()
+        $('.screen-icon').show()
+        $('.template1').hide()
+        $('.template2').show()
+    })
+
+    $('.screen-icon').on('click', function() {
+        $(this).hide()
+        $('.code-icon').show()
+        $('.template2').hide()
+        $('.template1').show()
+    })
+
+}
